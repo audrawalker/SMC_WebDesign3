db890035150cac4089555a9540359fe031efdcea
audra walker <walker-mueller_audri@student.smc.edu>
2020-04-02 19:00:02 -0700

    mid term dry run

---
+++ week 6 midterm/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //we write everything here//
+    $('.myCarousel').carousel()
+
+    $('#myModal').modal('show')
+    var myAlert = document.getElementById('myAlert')
+    $('#myAlert').alert('show')
+
+    
+
+});
