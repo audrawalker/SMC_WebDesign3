9bb76246164958aa6675728c86061a56d51cba09
audra walker <walker-mueller_audri@student.smc.edu>
2020-06-07 22:06:27 -0700

    FINAL

--- final/script.js
+++ final/script.js
@@ -1,4 +1,164 @@
-$(document).ready(function(){
+const text = document.querySelector(".navbar-brand");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+
+}
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if (char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [{
+        name: 'Black Hoodie',
+        tag: 'blackhoodie',
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: 'Grey Tshirt',
+        tag: 'greytshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'White Shirt',
+        tag: 'whiteshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Protest Gear',
+        tag: 'protetgear',
+        price: 50,
+        inCart: 0
+    },
+    {
+        name: 'Poster Equipment',
+        tag: 'posterequipment',
+        price: 25,
+        inCart: 0
+    },
+];
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i])
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product) {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+    setItems(product);
+
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    console.log('My cartItems are', cartItems);
+    if (cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+
+
+    localStorage.setItem("productInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+    //console.log("The product price is", product.price);
+    let cartCost = localStorage.getItem('totalCost');
+
+    console.log("My cartCost is", cartCost);
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+
+    } else {
+
+        localStorage.setItem("totalCost", product.price);
+
+    }
+}
+
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector 
+    (".products-container");
+
+    console.log(cartItems);
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = ''; 
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <img src="./assets/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            `
+        });
+
+    }
+
+}
+
+onLoadCartNumbers();
+displayCart();
+
+$(document).ready(function () {
     //we write everything here
 
     $('#myCarousel').carousel({
@@ -14,12 +174,12 @@ $(document).ready(function(){
                 type: "POST",
                 url: url,
                 data: $(this).serialize(),
-                success: function (data)
-                {
+                success: function (data) {
                     var messageAlert = 'alert-' + data.type;
                     var messageText = data.message;
 
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    var alertBox = '<div class="alert ' + messageAlert + 
+                    ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
                     if (messageAlert && messageText) {
                         $('#contact-form').find('.messages').html(alertBox);
                         $('#contact-form')[0].reset();
